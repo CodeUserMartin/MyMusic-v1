45e8f90f7ef9141f8edbf494fd9bf78535d80717
CodeUserMartin <martinpatole33@gmail.com>
2025-08-14 20:33:00 +0530

    added fetch song basic and new song folders

--- script.js
+++ script.js
@@ -4,4 +4,81 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const displaySongFolderContainerEl = document.getElementById('display-folders');
 
+
+    fetchSongs();
+
+
+    async function fetchSongs() {
+
+        try {
+
+            const url = `/songs/`
+            const response = await fetch(url);
+            const result = await response.text();
+
+
+            // console.log(result);
+
+            // Create an div to store the text of the result from the server
+
+            const div = document.createElement('div');
+
+            div.innerHTML = result;
+
+            let anchorTagFromText = div.getElementsByTagName('a');
+            console.log(anchorTagFromText);
+            console.log(typeof anchorTagFromText);
+
+            // Filtering out only the sub-folders inside the songs folder
+            let folderLink = [...anchorTagFromText].filter(a => a.classList.contains('icon-directory'));
+            console.log(folderLink);
+            let newFolders = folderLink.slice(1, folderLink.length);
+            console.log("newFolder: ", newFolders);
+
+            newFolders.forEach((folder) => {
+                renderFolder(folder);
+
+            });
+        } catch (error) {
+            console.log("fail to fetch songs", error);
+
+        }
+
+
+        // RenderFolders to the DOM
+
+        function renderFolder(folder) {
+
+            // displaySongFolderContainerEl.innerHTML = '';
+            displaySongFolderContainerEl.classList.remove('hidden');
+
+            let folderHref = folder.href;
+            let folderTitle = folder.title;
+
+
+            // Create an div element to store each folder
+
+            const folderDiv = document.createElement('div');
+
+            folderDiv.innerHTML = `
+           <div class="aspect-square w-[100px] md:w-[120px] lg:w-[150px] bg-black">
+                    <div>
+                    <p class="text-white">${folderTitle}</p>
+                    <img src="${folderHref}/cover.jpg" alt= "${folderTitle}">
+                    </div>
+                </div>
+            `
+
+            displaySongFolderContainerEl.appendChild(folderDiv);
+
+
+            folderDiv.addEventListener('click', () => {
+
+                window.location.href = folderHref;
+            })
+
+        }
+
+    }
+
 });
